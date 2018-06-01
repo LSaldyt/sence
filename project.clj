4330d988933fe136c03d455ead173eac3ebaeacd
Lucas Saldyt <lucassaldyt@gmail.com>
2018-06-01 06:48:39 -0700

    Adds paradex (and ari) imports to project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :url "github.com/LSaldyt/sence"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.2.4"]]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [ari "0.1.0"]
+                 [paradex "0.1.0"]]
   :plugins [[lein-bin "0.3.4"]
             [lein-marginalia "0.9.1"]]
   :bin { :name "ari" }
