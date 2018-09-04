6857b0ea1ea6a393dc7e3cec830ef0e9a2a6c76a
LSaldyt <lucassaldyt@gmail.com>
2018-09-04 08:32:27 -0700

    Adds old changes that were sitting around on my alternate machine

--- methods/heuristic.py
+++ methods/heuristic.py
@@ -1,6 +1,9 @@
 from .space import space, python_grammar, Sequence
 from .check import check, listify1, listify_seq
+from .known import seqsee_analyzed, known_lookup
 from .utils import flatten
+
+from .unix_timeout import timeout
 
 from pprint import pprint
 
@@ -65,11 +68,13 @@ def astar(branches, start, end, distance=distance):
     seen      = set()
     paths     = {to_seq(start) : start}
     heuristic = lambda node : paths[node].complexity() * distance(node, end)
+    tried = 0
 
     while tuple(end) not in paths:
         shortest = min(paths.keys(), key=heuristic)
-        smallest = heuristic(shortest)
-        print('{:<30} | {:<20} | {}'.format(str(shortest), str(smallest), paths[shortest]._code()))
+        #smallest = heuristic(shortest)
+        #print('{:<30} | {:<20} | {}'.format(str(shortest), str(smallest), paths[shortest]._code()))
+        tried += 1
 
         seen.add(shortest)
 
@@ -80,16 +85,42 @@ def astar(branches, start, end, distance=distance):
                 paths[to_seq(item)] = item
         del paths[shortest]
 
+        print('{}\r'.format(tried), end='', flush=True)
+
     shortest = min(paths.keys(), key=heuristic)
-    smallest = heuristic(shortest)
-    print('{:<30} | {:<20} | {}'.format(str(shortest), str(smallest), paths[shortest]._code()))
+    print('Found solution to {} after trying {} options'.format(end, tried))
+    #smallest = heuristic(shortest)
+    #print('{:<30} | {:<20} | {}'.format(str(shortest), str(smallest), paths[shortest]._code()))
 
     return paths[tuple(end)]
 
+
+def test_all(limit=20):
+    @timeout(limit)
+    def timed_test(problem):
+        astar(branches, Sequence(start='concat_def'), problem)
+    passed = set()
+    failed = set()
+    problems = list(known_lookup.keys())
+    print('Maximum time: {} seconds'.format(len(problems) * limit))
+    for problem in problems:
+        try:
+            timed_test(problem)
+            passed.add(problem)
+        except TimeoutError:
+            failed.add(problem)
+    print('Passed:')
+    pprint(passed)
+    print('Failed:')
+    pprint(failed)
+
 def heuristic():
-    problem = [2, 3, 4, 5]
-    problem = [2, 1, 2, 2, 2, 2, 2, 3, 2]
-    problem = [1, 1, 1, 2, 1, 3]
-    problem = [1, 4, 9, 16, 25, 36]
-    problem = [1, 1, 2, 1, 2, 3]
-    astar(branches, Sequence(start='concat_def'), problem)
+    #problem = [2, 3, 4, 5]
+    #problem = [2, 1, 2, 2, 2, 2, 2, 3, 2]
+    #problem = [1, 1, 1, 2, 1, 3]
+    #problem = [1, 4, 9, 16, 25, 36]
+    #problem = [1, 1, 2, 1, 2, 3]
+    #problem = [2, 4, 6, 8, 10]
+    #astar(branches, Sequence(start='concat_def'), problem)
+    test_all(limit=60)
+
