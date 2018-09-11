3c81b4461c23518f1e5684813df8a29f52b15f65
LSaldyt <lucassaldyt@gmail.com>
2018-09-11 15:14:35 -0700

    Revises tree search so that fibonacci numbers are guessable

--- methods/tree.py
+++ methods/tree.py
@@ -29,25 +29,34 @@ def enumerate_relations(inputs, outputs, answer, operators, considering=None):
                                   Rule([Partial(op, [RevIndex(inputs, i, 'inputs'), Index(0, 'arguments')], [op.f(inputs[i], outputs[j])])])))
                     rules.add(previous.join(
                               Rule([Partial(op, [RevIndex(inputs, i, 'inputs'), RevIndex(outputs, j, 'outputs')], [])])))
+                    for j2, y2 in enumerate(outputs):
+                        rules.add(previous.join(
+                                  Rule([Partial(op, [RevIndex(outputs, j, 'outputs'), RevIndex(outputs, j2, 'outputs')], [])])))
+                    for i2, x2 in enumerate(outputs):
+                        rules.add(previous.join(
+                                  Rule([Partial(op, [RevIndex(inputs, i, 'inputs'), RevIndex(inputs, i2, 'inputs')], [])])))
     return rules
 
 def tree(inputs, outputs, depth=1):
     considering = None
-    for i in range(1, len(inputs)):
-        for d in range(depth):
+    for d in range(depth):
+        for i in range(1, len(inputs)):
             local_inputs  = inputs[:i + 1]
             local_outputs = outputs[:i]
             local_answer  = outputs[i]
             proposed = enumerate_relations(local_inputs, local_outputs,
                                            answer=local_answer, operators=operators)
-            pprint(proposed)
+            #pprint(proposed)
             final = {rule for rule in proposed if rule.guess(local_inputs, local_outputs) == local_answer}
             if final:
                 guesses = {f.guess(inputs, outputs) for f in final}
+                correct = outputs[-1]
                 for f in final:
                     print(f, ' gives:')
-                    print(f.guess(inputs, outputs[:-1]))
+                    guess = f.guess(inputs, outputs[:-1])
+                    print(guess)
                     print('correct:', outputs[-1])
-                return
+                    if guess == correct:
+                        return
             else:
                 considering = proposed
