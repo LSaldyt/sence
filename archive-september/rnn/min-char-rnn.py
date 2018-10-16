50e8fd93e52ab5eab3162f9c9be89e063dc41401
LSaldyt <lucassaldyt@gmail.com>
2018-10-16 12:05:59 -0700

    Modifies rnn input

--- archive-september/rnn/min-char-rnn.py
+++ archive-september/rnn/min-char-rnn.py
@@ -6,7 +6,7 @@ BSD License
 import numpy as np
 
 # data I/O
-data = open('history_of_science.txt', 'r').read() # should be simple plain text file
+data = open('input.txt', 'r').read() # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
